7c1f634361edcbdfcf6610f3b475acb27d6718e8
jj <jorge.yong-ochoa.ext@syrocon.de>
2020-02-01 17:11:15 -0500

    Refactor the football data next games service to send pictures

--- src/main/java/com/jj/barcabot/service/football/FootballDataNextGames.java
+++ src/main/java/com/jj/barcabot/service/football/FootballDataNextGames.java
@@ -3,29 +3,38 @@ package com.jj.barcabot.service.football;
 import com.jj.barcabot.service.football.dto.NextMatchDto;
 import com.jj.barcabot.service.football.footballdata.FootballDataService;
 import com.jj.barcabot.service.serializer.NextMatchesSerializer;
-import com.jj.barcabot.service.telegram.MessageSender;
+import com.jj.barcabot.service.telegram.TelegramSender;
 import com.jj.barcabot.service.telegram.config.TelegramConfig;
+import gui.ava.html.image.generator.HtmlImageGenerator;
+import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 
 @Log4j2
 @Component
 @RequiredArgsConstructor
 public class FootballDataNextGames implements NextGamesService {
 
-  private final MessageSender sender;
+  private final TelegramSender sender;
   private final FootballDataService dataService;
   private final ModelMapper modelMapper;
   private final TelegramConfig config;
-  private final NextMatchesSerializer serializer;
+  private final NextMatchesSerializer nextMatchesHtmlSerializer;
 
   @Override
   public void publishNextGames() {
-    sender.sendMessage(config.getChannelId(), serializer.serialize(getNextMatches()));
+    sender.send(
+        buildPhoto(
+            config.getChannelId(),
+            convertHtmlToImage(
+                nextMatchesHtmlSerializer.serialize(
+                    getNextMatches()))));
   }
 
   private List<NextMatchDto> getNextMatches() {
@@ -33,6 +42,21 @@ public class FootballDataNextGames implements NextGamesService {
         .stream()
         .map(x -> modelMapper.map(x, NextMatchDto.class))
         .collect(Collectors.toList());
+  }
+
+  @SneakyThrows
+  private SendPhoto buildPhoto(String chatId, File image) {
+    SendPhoto photo = new SendPhoto();
+    photo.setChatId(chatId);
+    photo.setPhoto(image);
+    return photo;
+  }
+
+  private File convertHtmlToImage(String html) {
+    HtmlImageGenerator imageGenerator = new HtmlImageGenerator();
+    imageGenerator.loadHtml(html);
+    imageGenerator.saveAsImage("temp.png");
+    return new File("temp.png");
   }
 
 }
--- src/main/java/com/jj/barcabot/service/football/dto/NextMatchDto.java
+++ src/main/java/com/jj/barcabot/service/football/dto/NextMatchDto.java
@@ -3,7 +3,6 @@ package com.jj.barcabot.service.football.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jj.barcabot.service.football.footballdata.dto.Competition;
-import com.jj.barcabot.service.football.footballdata.dto.Season;
 import com.jj.barcabot.service.football.footballdata.dto.Team;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
@@ -15,7 +14,8 @@ public class NextMatchDto implements Serializable {
 
   private ZonedDateTime utcDate;
   private Competition competition;
-  private Season season;
+  private int matchDay;
+  private String stage;
   private Team homeTeam;
   private Team awayTeam;
 
